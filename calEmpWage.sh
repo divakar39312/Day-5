7e192f4a9a3e146cfeaaa0d29d0ed2053bd2d52e
Divakar <divakar39312@gmail.com>
2022-06-29 13:48:32 +0530

    second commit

--- calEmpWage.sh
+++ calEmpWage.sh
@@ -1,3 +1,4 @@
+#!/bin/bash 
 isPresent=1;
 random=$((RANDOM%2));
 if [ $isPresent -eq $random ];
@@ -5,6 +6,8 @@ then
 	empRatePerHr=20;
 	empHrs=8;
 	salary=$(($empHrs*$empRatePerHr));
+	echo "$salary"
 else
 	salary=0
+	echo "$salary"
 fi
